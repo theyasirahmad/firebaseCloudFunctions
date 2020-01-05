7d10b5177037efe19e2e8831e325cd338698d2a7
yasiir <yasira640@gmail.com>
2020-01-05 13:00:22 +0500

    comments of obseletion

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable consistent-return */
 const functions = require('firebase-functions');
-const gcs = require('@google-cloud/storage');
+const gcs = require('@google-cloud/storage')();// () calling method is now obselete
 const os = require('os');
 const path = require('path');
 // // Create and Deploy Your First Cloud Functions
@@ -9,9 +9,10 @@ const path = require('path');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-
+// onChange function was obselete thats why onFinalizes 
 exports.onFileChange = functions.storage.object().onFinalize(event => {
     // console.log(event);
+    // code does not work due to obseletion of some components i guess
     const object = event.data;
     const bucket = object.bucket;
     const contentType = object.contentType;
