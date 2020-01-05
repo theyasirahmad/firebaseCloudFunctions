2643e6d105af93875351ea16e27f5e688e3f617e
yasiir <yasira640@gmail.com>
2020-01-05 12:52:19 +0500

    onChangeFile Fuction for rename

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable consistent-return */
 const functions = require('firebase-functions');
-
+const gcs = require('@google-cloud/storage');
+const os = require('os');
+const path = require('path');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,7 +10,28 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
-exports.fileUpload = functions.storage.object().onFinalize(event=>{
-    console.log(event);
-    return;
+exports.onFileChange = functions.storage.object().onFinalize(event => {
+    // console.log(event);
+    const object = event.data;
+    const bucket = object.bucket;
+    const contentType = object.contentType;
+    const filePath = object.name;
+    console.log("File Change Detected, Fucntion execution started");
+
+    if(path.basename(filePath).startsWith('renamed-')){
+        console.log('already renamed file')
+       return;
+    }
+    const destBucket = gcs.Bucket(bucket);
+    const tempFilePath = path.join(os.tmpdir(), path.basename(filePath));
+    const metadata = { contentType: contentType };
+    return destBucket.file(filePath).download({
+        destination:tempFilePath
+    }).then(()=>{
+        console.log('we');
+        return destBucket.upload(tempFilePath, {
+            destination:'renamed-'+path.basename(filePath),
+            metadata:metadata
+        })
+    })
 });
